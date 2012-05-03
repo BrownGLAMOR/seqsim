fec81a774659b4eb61e05f1be876017def61ef66
jl52 <jl52@10.32.33.107>
2012-05-02 21:46:14 -0400

    MDP

--- src/LMSR/MDP.java
+++ src/LMSR/MDP.java
@@ -1,86 +1,74 @@
 package LMSR;
 
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
-import java.util.Map.Entry;
-import java.util.Random;
 import java.util.Set;
-
 import speed.IntegerArray;
 
-// TODO: This class still needs to be optimized for speed in populate() and getPMF().
-//       In particular, get rid of the need to make a new Integer[] for each call.
-//       Maybe use thread local storage?
-
-public class TransitionProb{
+// Stores transition probabilities P and rewards R for all agents in the game. Each state S or (S,A)-pair is an IntegerArray. 
+public class MDP{
 	
-	boolean take_log, ready = false;
+	boolean ready = false;
 	int no_agents, no_rounds, total_rounds;
 	
-	// Each state is an IntegerArray
-	
 	// One per agent
-	HashMap<IntegerArray, ArrayList<IntegerArray>>[] NextStates;	// A list of SA ==> further states
-//	HashMap<IntegerArray, double[]>[] P;							// Corresponding transition probs: SA ==> further states
-	HashMap<IntegerArray, Double>[] counts;							// num of times each state has been reached
-	HashMap<IntegerArray, Double>[] R;								// expected rewards
+	HashMap<IntegerArray, ArrayList<IntegerArray>>[] NextStates;	// [round]: (S,A) ==> further S. A list of further S
+	HashMap<IntegerArray, double[]>[] P;							// [round]: (S,A) ==> further S. Transition probs
+	HashMap<IntegerArray, Integer>[] counts;						// [round]: S. num of times S has been reached
+	HashMap<IntegerArray, Double>[] R;								// [round]: (S,A). expected rewards 
 	
-	// holder of things
+	// holder of (S,A)s
 	IntegerArray SA_tmp[];
 	
 	@SuppressWarnings("unchecked")
-	public TransitionProb(int no_agents, int no_rounds, boolean take_log) {
-		Cache.init();	// XXX: need this? 
-		this.take_log = take_log;
+	public MDP(int no_agents, int no_rounds) {
+
+		// game parameters
 		this.no_agents = no_agents;
 		this.no_rounds = no_rounds;
 		this.total_rounds = no_rounds*no_agents;
 		
-		// Possible actions
+		// Possible actions - HARDCODED
 		IntegerArray ActionSpace = new IntegerArray(new int[] {-1,0,1});
 				
-		// init this.P, this.P, this.R
+		// MDP mappings: initiate
 		this.NextStates = new HashMap[total_rounds];
-//		this.P = new HashMap[total_rounds];
 		this.counts = new HashMap[total_rounds];
+		this.P = new HashMap[total_rounds];
 		this.R = new HashMap[total_rounds];
 		
-		// Tally total number of possible histories... want to see them all hit
 		for (int i = 0; i < total_rounds; i++) {	
-			int max_states = (int) java.lang.Math.pow(ActionSpace.d.length, i); 
+			int max_states = (int) java.lang.Math.pow(ActionSpace.d.length, i+1);	// max no. of states in round i 
 			
 			this.NextStates[i] = new HashMap<IntegerArray, ArrayList<IntegerArray>>(max_states); 
-//			this.P[i] = new HashMap<IntegerArray, double[]>(max_realizations);
-			this.counts[i] = new HashMap<IntegerArray, Double>(max_states);
+			this.P[i] = new HashMap<IntegerArray, double[]>(max_states);
+			this.counts[i] = new HashMap<IntegerArray, Integer>(max_states);
 			this.R[i] = new HashMap<IntegerArray, Double>(max_states);
 		}
 		
-		// For temporarily holding states 
-		this.SA_tmp = new IntegerArray[total_rounds+1];
-		for (int i = 0; i < total_rounds + 1; i++)
-			this.SA_tmp[i] = new IntegerArray(i);
+		// initiate: temporary state holder
+		this.SA_tmp = new IntegerArray[total_rounds];
+		for (int i = 0; i < total_rounds; i++)
+			this.SA_tmp[i] = new IntegerArray(i+1);
 		
 	}
 		
 	// Call this to reset the internal state. This is automatically called when instantiated. 
 	public void reset() {
-		ready = false;
+		ready = false;		// not normalized yet
 		
 		// clean HashMaps
 		for (HashMap<IntegerArray, ArrayList<IntegerArray>> a : this.NextStates)
 			a.clear();
-//		for (HashMap<IntegerArray, double[]> a : this.P)
-//			a.clear();
-		for (HashMap<IntegerArray, Double> a : this.counts)
+		for (HashMap<IntegerArray, double[]> a : this.P)
+			a.clear();
+		for (HashMap<IntegerArray, Integer> a : this.counts)
 			a.clear();
 		for (HashMap<IntegerArray, Double> a : this.R)
 			a.clear();
-
 	}
 	
 	// Call this once per realized game
@@ -88,42 +76,72 @@ public class TransitionProb{
 		if (history.d.length != total_rounds || reward.length != total_rounds)
 			throw new RuntimeException("length game history must equal reward vector length");
 
-		// TODO: stopped here
-		
-		// Store all counts
-		for (int i = 0; i < total_rounds - no_agents; i++) {
+		// Store counts: no. of times a state is reached
+		for (int i = 0; i < total_rounds; i++){
 			IntegerArray SA = SA_tmp[i];
-			IntegerArray nextS = SA_tmp[i+no_agents-1];
-			
-			// SA --> nextS. Copy from "history"
-			for (int j = 0; j<i; j++){
+			for (int j = 0; j <= i; j++)
 				SA.d[j] = history.d[j];
-				nextS.d[j] = history.d[j];
-			}
-			for (int j = i; j < i+no_agents-1; j++)
-				nextS.d[j] = history.d[j];
-		
-			// get further states conditioned on earlier prices
-			ArrayList<IntegerArray> NS = NextStates[i].get(SA);
-						
-			if (NS == null) {
-				
-				// this is our first entry into the distribution. Initialize
-				NS = Cache.ExtensionStates(SA, no_agents);
-				double[] p = new double[NS.size()];
-				
-				for (int j = 0; j < p.length; j++)
-					p[j] = 0;
-				p[NS.indexOf(nextS)] = 1;
+			
+			if (!counts[i].containsKey(SA)){
+				// create this instance
+				counts[i].put(new IntegerArray(Arrays.copyOf(SA.d, SA.d.length)), 1);
+				R[i].put(new IntegerArray(Arrays.copyOf(SA.d, SA.d.length)), reward[i]);
 
-				// Don't have to do "new copy of" right? XXX
-				NextStates[i].put(SA, NS);
-				P[i].put(SA, p);
-			} else {
-				double[] p = P[i].get(SA);
-				p[NS.indexOf(nextS)] ++;
-			}			
+//				System.out.println("put R(SA" + SA.print() + ") = " + R[i].get(SA));
+//				System.out.println("put counts(SA" + SA.print() + ") = " + counts[i].get(SA));
+			} 
+			else {
+				R[i].put(new IntegerArray(Arrays.copyOf(SA.d, SA.d.length)), R[i].get(SA) + reward[i]);
+				counts[i].put(new IntegerArray(Arrays.copyOf(SA.d, SA.d.length)), counts[i].get(SA) + 1);
+				
+//				System.out.println("create new, put R(SA" + SA.print() + ") = " + R[i].get(SA));
+//				System.out.println("create new, put counts(SA" + SA.print() + ") = " + counts[i].get(SA));
+			}
+
 		}
+	}
+	
+	/*
+		// Call this once per realized game
+		public void populate(IntegerArray history, double[] reward) {
+			if (history.d.length != total_rounds || reward.length != total_rounds)
+				throw new RuntimeException("length game history must equal reward vector length");
+
+			// Store all counts
+			for (int i = 0; i < total_rounds - no_agents; i++) {
+				IntegerArray SA = SA_tmp[i];
+				IntegerArray nextS = SA_tmp[i+no_agents-1];
+				
+				// SA --> nextS. Copy from "history"
+				for (int j = 0; j<i; j++){
+					SA.d[j] = history.d[j];
+					nextS.d[j] = history.d[j];
+				}
+				for (int j = i; j < i+no_agents-1; j++)
+					nextS.d[j] = history.d[j];
+			
+				// get further states conditioned on earlier prices
+				ArrayList<IntegerArray> NS = NextStates[i].get(SA);
+							
+				if (NS == null) {
+					
+					// this is our first entry into the distribution. Initialize
+					NS = Cache.ExtensionStates(SA, no_agents);
+					double[] p = new double[NS.size()];
+					
+					for (int j = 0; j < p.length; j++)
+						p[j] = 0;
+					p[NS.indexOf(nextS)] = 1;
+
+					// Don't have to do "new copy of" right?
+					NextStates[i].put(SA, NS);
+					P[i].put(SA, p);
+				} else {
+					double[] p = P[i].get(SA);
+					p[NS.indexOf(nextS)] ++;
+				}			
+			}
+
 		
 		// Store all R
 		for (int i = 0; i < total_rounds; i++) {
@@ -137,11 +155,10 @@ public class TransitionProb{
 			if (! R[i].containsKey(SA))				
 				R[i].put(SA, reward[i]);
 			else
-				R[i].put(SA,R[i].get(SA) + reward[i]);		// XXX: does this rewrite the old entry?
+				R[i].put(SA,R[i].get(SA) + reward[i]);	
 		}
 	}
 	
-	/*
 	// Call this to do a weighted average of 2 MDPs, used in smoothing. TODO 
 	//	Basically, PNextStates[0] --> w*PNextStates[0] + (1-w)*PNextStates[1] 
 	public void addMDP(JointCondDistributionEmpirical jcde1, double w) {
@@ -149,7 +166,7 @@ public class TransitionProb{
 		// for all goods
 		for (int i = 0; i<total_rounds; i++) {			
 
-			// normalize conditional pdfs and update cdfs XXX: should we do this for all possible WRs? 
+			// normalize conditional pdfs and update cdfs 
 			for (IntegerArray SA : prob[i].keySet()) {
 				double[] p0 = prob[i].get(SA);
 				double[] p1 = jcde1.getPMF(SA);
@@ -184,72 +201,82 @@ public class TransitionProb{
 		}
 	*/
 	
-	// Call this to normalize P and R
+	// Call this to normalize/compute P and R
 	public void normalize() {
 
-		// Normalize P and R for first half
-		for (int i = 0; i < total_rounds - no_agents; i++) {			
-			for (IntegerArray SA : NextStates[i].keySet()) {
+		// Normalize P and R for first half of the game
+		for (int i = 0; i < total_rounds-no_agents; i++) {
+			
+//			System.out.println("normalizing for round i = " + i + ". counts[i].keySet.size() = " + counts[i].keySet().size());
+			for (IntegerArray SA : counts[i].keySet()) {				
 
-				// Get entities
-				double[] p = P[i].get(SA);
-				double r = R[i].get(SA);
+				// Normalize Rewards
+				R[i].put(SA,R[i].get(SA)/counts[i].get(SA));	// modify values, so don't need "copies of"
+//				System.out.println("R[" + i + "].put(" + SA.print() + "," + R[i].get(SA) + ")");
 				
-				// Get sum
-				double sum = 0.0;
-				for (int j = 0; j < p.length; j++)
-					sum += p[j];
+				// Probabilities: Figure out possible next round states
+				ArrayList<IntegerArray> NS = Cache.ExtensionStates(SA, no_agents);
 				
-				// Divide by sum 
-				r = r/sum;
+				// turn counts into pmf form
+				double[] p = new double[NS.size()];
 				for (int j = 0; j < p.length; j++)
-					p[j] /= sum;
+					p[j] = 0.0;
+
+				Iterator<IntegerArray> it = NS.iterator();
+				int sum = 0;
+				while (it.hasNext()){
+					IntegerArray nextS = it.next();
+					if (counts[i+no_agents-1].containsKey(nextS)) {
+						p[NS.indexOf(nextS)] = counts[i+no_agents-1].get(nextS);						
+						sum += counts[i+no_agents-1].get(nextS);
+					}
+				}
+				// Normalize
+				for (int j = 0; j < p.length; j++)
+					p[j] /= sum;				
+
+				// Put into memory
+//				NextStates[i].put(new IntegerArray(Arrays.copyOf(SA.d, SA.d.length)), NS);
+//				P[i].put(new IntegerArray(Arrays.copyOf(SA.d, SA.d.length)), p);				
+				NextStates[i].put(SA, NS);
+				P[i].put(SA, p);
 			}
 		}
-		
-	// Normalize R for the second half
-	for (int i = total_rounds - no_agents; i < total_rounds; i++) {			
-		for (IntegerArray SA : R[i].keySet()) {
-			
-			// Get entities
-			double r = R[i].get(SA); 
-
-			// Get sum and normalize
-			double sum = 0.0;
-			for (int j = 0; j < p.length; j++)
-				sum += p[j];
-			r = r/sum;
-		}
-	}
+			// Normalize R for second half of the game
+			for (int i = total_rounds-no_agents; i < total_rounds; i++) {			
+				// Normalize Rewards
+				for (IntegerArray SA : counts[i].keySet())
+					R[i].put(SA,R[i].get(SA)/counts[i].get(SA));
+			}
 	
-	ready = true;
+			ready = true;	// normalized
 }
 
-	// get Possible states to transition into from SA
+	// get Possible states to transition into from (S,A)
 	public ArrayList<IntegerArray> getNextStates(IntegerArray SA) {
-		return NextStates[SA.d.length].get(SA);
+		return NextStates[SA.d.length-1].get(SA);
 	}
 
-	// get transition probabilities from SA to further states
+	// get transition probabilities from (S,A) to further states
 	public double[] getP(IntegerArray SA) {
 
 		// Sanity check
 		if (!ready)
 			throw new RuntimeException("must normalize first");
-		if (SA.d.length >= total_rounds - no_agents)
+		if (SA.d.length > total_rounds - no_agents)
 			throw new IllegalArgumentException("no more rounds");
 		
 		// Get transition probabilities
-		double[] p = P[SA.d.length].get(SA);  
+		double[] p = P[SA.d.length-1].get(SA);  
 		
 		// TODO: Should use "near by states" when a (state,action) is not observed, not just an arbitrary one
 		if (p == null){
 			System.out.println("warning: (state,action) = " + SA.print() + " not observed");
 
 			// Use the first one that comes up
-			if (P[SA.d.length].entrySet().size() > 0){
+			if (P[SA.d.length-1].entrySet().size() > 0){
 				Iterator<IntegerArray> it = P[SA.d.length].keySet().iterator();
-				p = P[SA.d.length].get(it.next());
+				p = P[SA.d.length-1].get(it.next());
 			}else{
 				throw new IllegalArgumentException("Nothing observed at all for the round of (state,action) = " + SA.print());
 			}
@@ -267,13 +294,11 @@ public class TransitionProb{
 		if (SA.d.length >= total_rounds)
 			throw new IllegalArgumentException("no more rounds");
 
-		if (R[SA.d.length].containsKey(SA))
-			return R[SA.d.length].get(SA);
+		if (R[SA.d.length-1].containsKey(SA))
+			return R[SA.d.length-1].get(SA);
 		else
 			return 0.0;		// TODO: something to tweak. Set it artificially high so that all states are explored? 
 	}
-	
-	
 
 	/* might want in the future
 	public void outputRaw(FileWriter fw) throws IOException {
@@ -346,7 +371,6 @@ public class TransitionProb{
 	}
 		*/
 
-
 	// Testing
 	public static void main(String args[]) throws IOException {
 
@@ -360,12 +384,21 @@ public class TransitionProb{
 		// Manually populate MDP
 		Set<IntegerArray> histories = Cache.genStates(total_rounds);
 		double[] reward = new double[total_rounds];
-		for (int i = 0; i < reward.length; i++)
-			reward[i] = 1.0;
 		
-		TransitionProb mdp = new TransitionProb(no_agents, no_rounds, false);
-		for (IntegerArray hist: histories)
+		MDP mdp = new MDP(no_agents, no_rounds);
+		for (IntegerArray hist: histories){
+			// rewards = simply cumulative sum of action history
+			reward[0] = hist.d[0];
+			for (int i = 1; i < reward.length; i++)
+				reward[i] = reward[i-1] + hist.d[i];
 			mdp.populate(hist, reward);
+		}
+
+		// create bias of self-transition between 0s
+		int bias = 100;
+		for (int i = 0; i < bias; i++)
+			mdp.populate(new IntegerArray(new int[] {0,0,0,0}), new double[] {0,0,0,0});
+		
 		mdp.normalize();
 		
 		// Test get next states, getP, and getR
@@ -374,8 +407,6 @@ public class TransitionProb{
 		SAs.add(new IntegerArray(new int[] {0}));
 		SAs.add(new IntegerArray(new int[] {1}));
 		SAs.add(new IntegerArray(new int[] {1,1}));
-		SAs.add(new IntegerArray(new int[] {0,0,0}));
-		SAs.add(new IntegerArray(new int[] {-1,-1,-1,-1}));
 		
 		IntegerArray SA;
 		Iterator<IntegerArray> it = SAs.iterator();
@@ -388,7 +419,7 @@ public class TransitionProb{
 			double r = mdp.getR(SA);
 			
 			// print SA and reward
-			System.out.println("SA = " + SA.print() + ": r = " + r);
+			System.out.println("\nSA = " + SA.print() + ": r = " + r);
 			
 			// print next round states
 			System.out.print("NSs = ");
